a0bbe426d1f7dc380b7f642cce9469dedcc164cb
Soham Bhattacharyya <boomsoham@gmail.com>
2024-12-30 11:01:15 +0530

    second commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'QApplication',
     version= '0.0.1',
-    author= 'sunny savita',
-    author_email= 'sunny.savita@gmail.com',
+    author= 'Soham Bhattacharyya',
+    author_email= 'boomsoham@gmail.com',
     packages= find_packages(),
     install_requires = []
 
